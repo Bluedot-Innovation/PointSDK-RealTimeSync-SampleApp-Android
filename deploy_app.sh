c5fcc42ecef4530d57c77500ee70b045fc3e2ef2
jp <jamie@bluedot.io>
2019-10-02 16:38:47 +1000

    Prepare repo for GitHub publication.

--- deploy_app.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-NOTES=$(<ReleaseNotes.txt)
-/usr/bin/curl -F "status=2" -F "notify=2" -F ipa=@"/workspace/app/build/outputs/apk/debug/app-debug.apk" -F notes="$NOTES" -H "X-HockeyAppToken: 27d8525f6ff442348ed0a07e39605a0c" https://rink.hockeyapp.net/api/2/apps/upload
